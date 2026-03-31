47b5eea8f9d4770453444e873a5e822e02751be8
Noman Azhar <noman@maxguard.ae>
2026-03-31 10:50:28 +0400

    fix picture tag

--- application/views/iv-therapy.php
+++ application/views/iv-therapy.php
@@ -56,7 +56,7 @@
     <section class="sub-banner">
              <!-- <img src="<?= base_url() ?>assets/frontend/img/ocean.webp" alt="Premium IV Therapy at home in Dubai" fetchpriority="high" loading="eager" width="100%" height="600" style="height:600px; width:100%; object-fit:cover; object-position: bottom;"> -->
 
-            <picture>
+            <picture style="width: 100%;">
               <!-- Mobile -->
               <source media="(max-width: 768px)" 
                       srcset="<?= base_url() ?>assets/frontend/img/ocean-mobile.webp">
